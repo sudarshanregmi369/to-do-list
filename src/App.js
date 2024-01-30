f876406a9359ec1b24a3613e6653d453d204d5d9
i-m-sudarshan <regmisudarshan8@gmail.com>
2024-01-30 18:39:16 +0545

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './App.css'
 import SearchBox from './component/SearchBox'
+import AddRemove from  './component/AddRemove'
 const App=()=>{
   return(
     <div className='main-container '>
     <div className='central-container'>
-    <SearchBox />
-    <addRemove/>
+    <SearchBox/>
+    <AddRemove/>
     </div>
-    
     
     </div>
   )
